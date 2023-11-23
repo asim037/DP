cf67d3b3be9da357d6b9e7ca606b079f7594e7e2
Asim Tariq <107315975+asim037@users.noreply.github.com>
2023-11-23 10:30:00 +0500

    Update Fish.java

--- DesignPatterns/GOF/src/gof/prototype/shape/Fish.java
+++ DesignPatterns/GOF/src/gof/prototype/shape/Fish.java
@@ -8,11 +8,10 @@ package gof.prototype.shape;
  *
  * @author LAPTOP HOUSE
  */
-public class Fish extends Shape {
+public class Fish extends Shape{
     public Fish() {
         type = "Fish";
     }
-
     @Override
     public void draw() {
         System.out.println("Inside Fish::draw() method.");
