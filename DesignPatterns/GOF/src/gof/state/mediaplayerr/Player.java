2363cbad804fc3f54d1e2176d17eee6333c705c3
Asim Tariq <107315975+asim037@users.noreply.github.com>
2023-12-09 21:17:01 +0500

    updated

--- DesignPatterns/GOF/src/gof/state/mediaplayerr/Player.java
+++ DesignPatterns/GOF/src/gof/state/mediaplayerr/Player.java
@@ -16,6 +16,7 @@ public class Player {
     private boolean playing = false;
     private List<String> playlist = new ArrayList<>();
     private int currentTrack = 0;
+    private TrackIterator trackIterator;
 
     public Player() {
         this.state = new ReadyState(this);
@@ -23,6 +24,7 @@ public class Player {
         for (int i = 1; i <= 12; i++) {
             playlist.add("Track " + i);
         }
+        this.trackIterator = new TrackIterator(playlist);
     }
 
     public void changeState(State state) {
@@ -42,23 +44,49 @@ public class Player {
     }
 
     public String startPlayback() {
-        return "Playing " + playlist.get(currentTrack);
+        return "Playing " + trackIterator.getCurrentTrack();
+//        return "Playing " + trackIterator.get(currentTrack);
     }
 
     public String nextTrack() {
-        currentTrack++;
-        if (currentTrack > playlist.size() - 1) {
-            currentTrack = 0;
-        }
-        return "Playing " + playlist.get(currentTrack);
+        String track = trackIterator.nextTrack();
+        checkForTrackDamage(track);
+        return "Playing " + track;
+//        currentTrack++;
+//        if (currentTrack > playlist.size() - 1) {
+//            currentTrack = 0;
+//        }
+//        return "Playing " + playlist.get(currentTrack);
     }
 
     public String previousTrack() {
-        currentTrack--;
-        if (currentTrack < 0) {
-            currentTrack = playlist.size() - 1;
+        String track = trackIterator.previousTrack();
+        checkForTrackDamage(track);
+        return "Playing " + track;
+//        currentTrack--;
+//        if (currentTrack < 0) {
+//            currentTrack = playlist.size() - 1;
+//        }
+//        return "Playing " + playlist.get(currentTrack);
+    }
+    
+    private void checkForTrackDamage(String track) {
+        if (track.equals("Track 5") && playing) {
+            // Simulating a delay of 3 seconds
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            stopPlayback();
+            // Alert user to reset the player
+            System.out.println("Player is damaged. Please reset.");
         }
-        return "Playing " + playlist.get(currentTrack);
+    }
+    
+    public void stopPlayback() {
+        setPlaying(false);
+        trackIterator.reset();
     }
 
     public void setCurrentTrackAfterStop() {
