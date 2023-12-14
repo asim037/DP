7dc8b4a296bd39844377ecf75317f0100cdd6ed2
Asim Tariq <107315975+asim037@users.noreply.github.com>
2023-12-14 10:58:30 +0500

    updated

--- DesignPatterns/GOF/src/gof/memento/editor/Main.java
+++ DesignPatterns/GOF/src/gof/memento/editor/Main.java
@@ -6,19 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Editor editor = new Editor();
         editor.loadShapes(
-                new Circle(10, 10, 10, Color.BLUE),
+                new Circle(15, 15, 15, Color.BLUE),
 
                 new CompoundShape(
-                        new Circle(110, 110, 50, Color.RED),
-                        new Dot(160, 160, Color.RED)
+                        new Circle(120, 120, 60, Color.RED),
+                        new Dot(170, 170, Color.RED)
                 ),
 
                 new CompoundShape(
-                        new Rectangle(250, 250, 100, 100, Color.GREEN),
-                        new Dot(240, 240, Color.GREEN),
-                        new Dot(240, 360, Color.GREEN),
-                        new Dot(360, 360, Color.GREEN),
-                        new Dot(360, 240, Color.GREEN)
+                        new Rectangle(260, 260, 120, 120, Color.GREEN),
+                        new Dot(250, 250, Color.GREEN),
+                        new Dot(250, 380, Color.GREEN),
+                        new Dot(380, 380, Color.GREEN),
+                        new Dot(380, 250, Color.GREEN)
+                ),
+                new CompoundShape(
+                        new Oval(480, 480, 100, 60, Color.ORANGE),
+                        new Dot(520, 505, Color.ORANGE)
                 )
         );
     }
